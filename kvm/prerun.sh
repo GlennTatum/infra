3a479042982466fcd98d37ecae2e383963796d21
GlennTatum <glennetatum@gmail.com>
2025-09-08 23:00:59 -0400

    resize kvm images

--- kvm/prerun.sh
+++ kvm/prerun.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-1.img
-cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-2.img
-cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-3.img
-cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-4.img
-cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-5.img
+for ((IMG=1; IMG <= 5; IMG++)); do
+    cp noble-server-cloudimg-amd64.img noble-server-cloudimg-amd64-$IMG.img
+    qemu-img resize noble-server-cloudimg-amd64-$IMG.img 15G
+done
